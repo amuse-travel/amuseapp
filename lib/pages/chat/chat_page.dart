2ae49a714891241135914606fcf4446580df734f
kmh339 <kmh339@gmail.com>
2020-10-12 14:10:59 +0900

    update gitignore change sender color

--- lib/pages/chat/chat_page.dart
+++ lib/pages/chat/chat_page.dart
@@ -1,17 +1,26 @@
 import 'package:dash_chat/dash_chat.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
+  const ChatPage({
+    @required this.userName,
+  }) : assert(userName != null);
+
+  final String userName;
+
   @override
   _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  String _userName;
+
   final GlobalKey<DashChatState> _chatViewKey = GlobalKey<DashChatState>();
 
-  final ChatUser _chatUser = ChatUser(
-    name: 'Test',
-  );
+  TextEditingController _textEditingController;
+
+  ChatUser _chatUser;
 
   final ChatUser _otherUser = ChatUser(
     name: 'Other',
@@ -27,9 +36,17 @@ class _ChatPageState extends State<ChatPage> {
   void initState() {
     super.initState();
 
+    _userName = widget.userName;
+
+    _textEditingController = TextEditingController();
+
+    _chatUser = ChatUser(
+      name: _userName,
+    );
+
     _messages.add(
       ChatMessage(
-        text: 'Welcome to Amuse world!',
+        text: 'Welcome to Amuse world!\n$_userName!',
         createdAt: DateTime(2020, 10, 1),
         user: _otherUser,
       ),
@@ -44,8 +61,15 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
+  Color _senderColor() {
+    if (_textEditingController.text.isNotEmpty) {
+      return Colors.blue;
+    } else {
+      return Colors.grey;
+    }
+  }
+
   void _onSend(ChatMessage message) {
-    print(message.createdAt);
     _messages.add(message);
 
     _chatViewKey.currentState.scrollController.animateTo(
@@ -60,6 +84,7 @@ class _ChatPageState extends State<ChatPage> {
     final double _sizeWidth = MediaQuery.of(context).size.width;
     final double _sizeHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Chat'),
         centerTitle: true,
@@ -68,24 +93,54 @@ class _ChatPageState extends State<ChatPage> {
         child: Container(
           width: _sizeWidth,
           height: _sizeHeight,
-          color: Colors.black,
           child: DashChat(
             key: _chatViewKey,
             user: _chatUser,
             messages: _messages,
+            textController: _textEditingController,
+            inputMaxLines: 3,
             onSend: _onSend,
             sendOnEnter: true,
             textInputAction: TextInputAction.send,
             scrollToBottom: true,
             scrollToBottomStyle: ScrollToBottomStyle(
-              backgroundColor: Colors.lightBlue,
+              backgroundColor: Colors.lightBlue.withOpacity(0.9),
             ),
             shouldShowLoadEarlier: false,
             onLoadEarlier: () {
-              print('reaches top');
+              print('reached top');
             },
             showUserAvatar: false,
             showAvatarForEveryMessage: false,
+            inputContainerStyle: const BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 0.5,
+                  color: Colors.grey,
+                ),
+              ),
+              color: Colors.white,
+            ),
+            sendButtonBuilder: (Function sendMessage) => IconButton(
+              onPressed: () => sendMessage(),
+              icon: Icon(
+                Icons.send,
+                color: _senderColor(),
+              ),
+            ),
+            // onTextChange: _onTextChanged,
+            messageDecorationBuilder: (ChatMessage chatMessage, bool isUser) {
+              if (isUser) {
+                return BoxDecoration(
+                  color: Colors.blue[400],
+                );
+              } else {
+                return BoxDecoration(
+                  color: Colors.grey[200],
+                );
+              }
+            },
+            dateFormat: DateFormat('yyyy.MM.dd'),
           ),
         ),
       ),
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,14 +1,39 @@
 import 'package:amuse_app/pages/chat/chat_page.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  void _onLoginButtonPressed(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute<Widget>(
-        builder: (BuildContext context) => ChatPage(),
-      ),
-    );
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+
+    super.dispose();
+  }
+
+  void _onLoginButtonPressed() {
+    if (_textEditingController.text.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute<Widget>(
+          builder: (BuildContext context) => ChatPage(
+            userName: _textEditingController.text,
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -16,6 +41,7 @@ class LoginPage extends StatelessWidget {
     final double _sizeWidth = MediaQuery.of(context).size.width;
     final double _sizeHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
           width: _sizeWidth,
@@ -27,8 +53,17 @@ class LoginPage extends StatelessWidget {
                 child: const Text('Amuse App'),
               ),
               Container(
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                child: TextFormField(
+                  controller: _textEditingController,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
                 child: RaisedButton(
-                  onPressed: () => _onLoginButtonPressed(context),
+                  onPressed: _onLoginButtonPressed,
                   child: const Text(
                     'Login',
                   ),
