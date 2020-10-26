296921062f84751d78ad425143a407aabc7b539d
kmh339 <kmh339@gmail.com>
2020-10-26 13:38:14 +0900

    add update nickname page

--- lib/pages/setting/setting_page.dart
+++ lib/pages/setting/setting_page.dart
@@ -1,8 +1,18 @@
 import 'package:amuse_app/blocs/authentication/authentication_bloc.dart';
+import 'package:amuse_app/pages/setting/update_nickname/update_nickname_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
+  void _updateNickname(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute<Widget>(
+        builder: (BuildContext buildContext) => UpdateNicknamePage(),
+      ),
+    );
+  }
+
   void _logout(BuildContext context) {
     context.bloc<AuthenticationBloc>().add(AuthenticationOut());
   }
@@ -15,6 +25,14 @@ class SettingPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Container(
+            width: 100,
+            child: RaisedButton(
+              onPressed: () => _updateNickname(context),
+              color: Colors.white,
+              child: const Text('닉네임 변경'),
+            ),
+          ),
           Container(
             width: 100,
             child: RaisedButton(
