ed9f8855bd093ab0d58b78f66aa83e7c0457bbc0
kmh339 <kmh339@gmail.com>
2020-10-27 11:56:15 +0900

    change splash image

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -5,15 +5,15 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width:  MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Theme.of(context).primaryColor,
-        child: const Center(
-          child: Text(
-            'Welcome To Amuse',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            child: Image.asset(
+              'assets/icons/amuse-mark.png',
             ),
           ),
         ),
