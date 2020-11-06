95329779349da11629ee5d87e53a2938f6c1e147
kmh339 <kmh339@gmail.com>
2020-11-06 14:33:22 +0900

    ,

--- lib/pages/setting/terms_of_service/terms_of_service_page.dart
+++ lib/pages/setting/terms_of_service/terms_of_service_page.dart
@@ -19,6 +19,80 @@ class TermsOfServicePage extends StatelessWidget {
         elevation: 1,
         backgroundColor: Colors.white,
       ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _SliverPersistentHeaderDelegate(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.white,
+                child: const Text(
+                  '국내 여행 약관',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: MEDIUM,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  child: Text('제1조 (목적)',
+                  style: TextStyle(fontSize: 12),),
+                ),
+              ],
+            ),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _SliverPersistentHeaderDelegate(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 150.0,
+            delegate: SliverChildListDelegate(
+              [
+                Container(color: Colors.red),
+                Container(color: Colors.purple),
+                Container(color: Colors.green),
+                Container(color: Colors.orange),
+                Container(color: Colors.yellow),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class _SliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  _SliverPersistentHeaderDelegate({
+    @required this.child,
+  }) : assert(child != null);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => 50;
+
+  @override
+  double get minExtent => 50;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => false;
 }
